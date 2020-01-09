793f9c34ad55ef23c4374ccbbcefd7a2f547bc08
Malav Dhalgara <mdhalgar@centralstatesfunds.org>
2020-01-09 13:32:25 -0600

    Update

--- publish.cmd
+++ publish.cmd
@@ -1,3 +1,3 @@
 nuget restore
-msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=csf-web-qna-bot-Web-Deploy.pubxml -p:Password=EEwmJ58mbbgK8doyTrguMm4QG9icgX1jhX5vCCiBfkvaBJakTi9rwYEmhsS2
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=csf-web-qna-bot-bot-Web-Deploy.pubxml -p:Password=MG095ELhuGluXjRlnF84wHxpWu8F7vtJtk2C74X0AoLerksGveg2AtGmQAeD
 
